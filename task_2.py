6d7da9edd3bde18a17fcaaa791e420b07806590d
Saint-Blade <147116330+ahnafyura@users.noreply.github.com>
2025-12-01 22:44:24 +0700

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -2,46 +2,46 @@ import abc
 from abc import ABC, abstractmethod
 
 class Product(ABC): 
-    def __init__(self, ProductName):
-        self._ProductName = ProductName
+    def __init__(self, product_name):
+        self._product_name = product_name
     
     @property
-    def Name(self):
-        return self._ProductName
+    def name(self):
+        return self._product_name
     
     @abstractmethod
     def get_info(self):
         pass
 
 class Drink(Product):
-    def __init__(self, ProductName, Mililiters):
-        super().__init__(ProductName)
-        self._Mililiters = Mililiters
+    def __init__(self, product_name, volume_ml):
+        super().__init__(product_name)
+        self._volume_ml = volume_ml
 
     def get_info(self):
-        return f"{self._Mililiters} ml"
+        return f"{self._volume_ml} ml"
         
 class Snack(Product):
-    def __init__(self, ProductName, Calories):
-        super().__init__(ProductName)
-        self._Calories = Calories
+    def __init__(self, product_name, calories):
+        super().__init__(product_name)
+        self._calories = calories
 
     def get_info(self):
-        return f"{self._Calories} kcal"
+        return f"{self._calories} kcal"
     
-class VindingMachine:
+class VendingMachine: 
     def __init__(self):
-        self._Product = []
+        self._product_list = []
 
-    def addProduct(self, Product):
-        self._Product.append(Product)
+    def add_product(self, product):
+        self._product_list.append(product)
     
-    def Display(self):
+    def display_products(self):
         w_name = 20 
         w_info = 15
-        
         pembatas = f"+{'-' * (w_name + 2)}+{'-' * (w_info + 2)}+"
 
+        print("\n VENDING MACHINE ")
         print(pembatas)
         print(f"| {'Product Name':<{w_name}} | {'Information':>{w_info}} |")
         print(pembatas)
@@ -49,13 +49,13 @@ class VindingMachine:
         total_mililiters = 0
         total_calories = 0
 
-        for item in self._Product:
-            print(f"| {item.Name:<{w_name}} | {item.get_info():>{w_info}} |")
+        for item in self._product_list:
+            print(f"| {item.name:<{w_name}} | {item.get_info():>{w_info}} |")
             
             if isinstance(item, Drink):
-                total_mililiters += item._Mililiters
+                total_mililiters += item._volume_ml
             elif isinstance(item, Snack):
-                total_calories += item._Calories
+                total_calories += item._calories
 
         print(pembatas)
         print(f"| {'TOTAL Drink':<{w_name}} | {total_mililiters:>12} ml |")
@@ -64,24 +64,36 @@ class VindingMachine:
         print(" ")
 
 if __name__ == "__main__":
-    Vinding_Machine = VindingMachine()
-    
-    n = int(input())
+    my_machine = VendingMachine()
+    try:
+        raw_n = input()
+        if not raw_n:
+            n = 0
+        else:
+            n = int(raw_n)
+    except ValueError:
+        print("Inputnya harus angka gng")
+        exit()
 
     for _ in range(n):
         data = input().split() 
+        
+        if len(data) < 3:
+            continue
 
-        TypeProduct = data[0]
-        ProductName = data[1]
+        type_product = data[0]
+        product_name = data[1]
         value = int(data[2])
 
-        if TypeProduct.lower() in ["drink", "d"]:
-            drinkItem = Drink(ProductName, value)
-            Vinding_Machine.addProduct(drinkItem)
-        elif TypeProduct.lower() in ["snack", "s"]:
-            SnackItem = Snack(ProductName, value)
-            Vinding_Machine.addProduct(SnackItem)
+        if type_product.lower() in ["drink", "d"]:
+            item = Drink(product_name, value)
+            my_machine.add_product(item)
+            
+        elif type_product.lower() in ["snack", "s"]:
+            item = Snack(product_name, value)
+            my_machine.add_product(item)
+            
         else:
-            print(f"Tipe '{TypeProduct}' pada produk '{ProductName}' tidak dikenali. Produk dilewati")
+            print(f"⚠ Tipe '{type_product}' tipenya ngga ada deh, aku skip ya.")
 
-    Vinding_Machine.Display()
+    my_machine.display_products()
